5d66e1b8c83f57416e00a28ba52d3d41278c1052
mylestwyman <mct2008mct@gmail.com>
2026-07-17 15:20:12 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav-links');
-if (toggle && nav) toggle.addEventListener('click', () => nav.classList.toggle('open'));
-
-const year = document.querySelectorAll('[data-year]');
-year.forEach(el => el.textContent = new Date().getFullYear());
